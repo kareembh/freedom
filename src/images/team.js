e54285c5e02964c651964bd81c4f266adc8b2956
Kareem Brathwaite-Henry <kareembrathwaite-henry@Kareems-MacBook-Pro.local>
2022-08-01 17:57:34 -0400

    added page components to project

---
+++ src/images/team.js
@@ -0,0 +1,16 @@
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const Team = () => {
+  return (
+    <main>
+      <title>Our Team</title>
+      <h1>Team</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default Team
---
+++ src/pages/services.js
@@ -0,0 +1,16 @@
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const Services = () => {
+  return (
+    <main>
+      <title>Our Services</title>
+      <h1>Services</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default Services
